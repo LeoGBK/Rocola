a52f82569e5fce45af3f0490a6e8e39305055444
L€0 <69213766+LeoGBK@users.noreply.github.com>
2025-02-12 22:47:21 +0100

    Creating a class Rocola and a list with songs

---
+++ rocola.js
@@ -0,0 +1,18 @@
+class Rocola {
+
+    temas = [
+        "Stairway To Heaven",
+        "Light My Fire",
+        "No Me Sejan Salir",
+        "Persiana Americana",
+        "Eres",
+        "Strobe",
+        "Bangarang",
+        "Te recuerdo Amanda"
+    ];
+
+    play(k) {
+        let tema;
+        return tema;
+    }
+}
